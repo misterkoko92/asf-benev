f76f1c1ce954cc47520e4ba720fc9949c5f5019c
Edouard Gonnu <edouard.gonnu@gmail.com>
2026-01-02 15:33:29 +0100

    Add SMTP timeout and SSL settings

--- asf_benev/settings.py
+++ asf_benev/settings.py
@@ -103,6 +103,8 @@ EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "1") == "1"
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "0") == "1"
+EMAIL_TIMEOUT = int(os.getenv("EMAIL_TIMEOUT", "10"))
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "no-reply@example.com")
 
 CSRF_TRUSTED_ORIGINS = [
